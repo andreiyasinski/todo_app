d497884ac48c49d917408278281b1081c7a19198
Andrei Yasinski <andrewtopol856@gmail.com>
2019-02-05 22:31:20 +0300

    add todosData

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import TodoItem  from '././components/TodoItem';
+import todosData from './todosData'
 
 class App extends Component {
   render() {
+    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
     return (
       <div className='todo-list'>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todoItems}
       </div>
     );
   }
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 export default class TodoItem extends Component {
-  render() {
+  render(props) {
     return (
       <div className='todo-item'>
-        <input type="checkbox" />
-        <p>Placeholder text here</p>
+        <input type="checkbox" checked={this.props.item.completed} />
+        <p>{this.props.item.text}</p>
       </div>
     );
   }
