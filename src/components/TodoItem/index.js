844be9608ba83b01bcbb3c29bc9cc6ec9b991935
Andrei Yasinski <andrewtopol856@gmail.com>
2019-02-20 21:34:41 +0300

    fix issue wish checkbox

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 
 export default class TodoItem extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className='todo-item'>
-        <input type="checkbox" checked={this.props.item.completed} />
+        <input type="checkbox" 
+          checked={this.props.item.completed} 
+          onChange={() => console.log('Changed')}
+        />
         <p>{this.props.item.text}</p>
       </div>
     );
